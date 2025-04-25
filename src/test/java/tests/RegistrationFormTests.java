7e4cd457ed16ca29ef287370f1d0cbc0d544b3c5
Ivan Budenkov <ibudenkov@alfabank.ru>
2025-04-25 18:41:05 +0300

    HW. Lesson 6

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-public class RegistrationWithPageObjectsTests extends TestBase {
+public class RegistrationFormTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
