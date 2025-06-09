1e6d106839d93573e7f1ac5811f61279d5388c63
Ivan Budenkov <ibudenkov@alfabank.ru>
2025-06-09 14:22:40 +0300

    HW. Lesson 12. Добавил System.getProperty

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,11 +14,11 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser.version", "137.0");
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
-
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
